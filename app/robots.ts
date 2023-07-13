f75c6ac6a5d071bd64909679c867d11bdafe9e4c
tiwariaditya15 <40066667+tiwariaditya15@users.noreply.github.com>
2023-07-13 21:23:00 +0530

    sitrmap error

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,3 @@
-const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : 'http://localhost:3000';
-
 export default function robots() {
   return {
     rules: [
